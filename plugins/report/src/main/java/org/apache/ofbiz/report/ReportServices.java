c6e2251abaccb3a64b6fe71a2f5a4ea88a187a35
minuk <qpd3528@gmail.com>
2023-03-08 11:11:39 +0900

    ofbiz_11

--- plugins/report/src/main/java/org/apache/ofbiz/report/ReportServices.java
+++ plugins/report/src/main/java/org/apache/ofbiz/report/ReportServices.java
@@ -72,6 +72,19 @@ public class ReportServices {
 
         return result;
     }
+
+    public static Map<String, Object> listReportType(DispatchContext ctx, Map<String, Object> context) throws GenericEntityException {
+        Delegator delegator = ctx.getDelegator();
+
+        List<GenericValue> list = EntityQuery.use(delegator).from("ReportType").orderBy("reportTypeId").cache(true).queryList();
+
+        Map<String, Object> result = ServiceUtil.returnSuccess();
+
+        result.put("list", list);
+
+        return result;
+    }
+
     private static String getMessage() {
         return null;
     }
